b13d099d093ee181f679ebe102b39e8d595abae6
Josh Mancuso <30558826+Nolanole@users.noreply.github.com>
2019-10-07 17:37:28 -0500

    uploaded recursive sorting + search stretch goal

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,25 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(A, B):
+    merged_arr = []
     # TO-DO
-    
+    while len(A) + len(B) > 0:
+      if len(A) == 0:
+        merged_arr.append(B.pop(0))
+      elif len(B) == 0:
+        merged_arr.append(A.pop(0))
+      else:
+        if A[0] < B[0]:
+          merged_arr.append(A.pop(0))
+        else:
+          merged_arr.append(B.pop(0))
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
-
-    return arr
+def merge_sort(l):
+    A, B = l[:len(l)//2], l[len(l)//2:]
+    while len(A) > 1 or len(B) > 1:
+        return merge(merge_sort(A), merge_sort(B))
+    return merge(A,B)
 
 
 # STRETCH: implement an in-place merge sort algorithm
